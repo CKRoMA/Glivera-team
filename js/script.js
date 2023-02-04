3aa6facdfa291c973a64ee2d6188656522f5947b
Roman Pavlenko <1@1.com>
2023-02-04 16:03:54 +0300

    work is over

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 // "use strict" // строгий режим
+const links = document.querySelectorAll('.menu-dashboard__inner');
 
-// document.addEventListener('DOMContentLoaded', function () { //проверка док-т уже загружен
+for (let link of links) {
+	link.addEventListener('click', () => {
+		let activeLink = document.querySelector('.menu-dashboard__inner._active');
+		if (activeLink) {
+			activeLink.classList.remove('_active');
+		}
+		link.classList.add('_active');
+	})
+}
 
-// };
-
-$(function () {
-
-});
 
 
